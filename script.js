3e4632f37f505dbec46636c91b6680be0527ca9d
shumez <stanis@netvolution.fr>
2021-11-15 11:06:23 +0100

    Amélioration de l'appel Ajax

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const bouton = document.getElementById("bouton");
 const donnees = document.getElementById("donnees");
 
-const url = "http://www.filltext.com/?rows=10&prenom={firstName}&nom={lastName}";
+const url = "http://www.filltext.com/?rows=10&prenom={firstName}&nom={lastName}&age={numberRange|18, 62}";
 
 bouton.addEventListener("click", () => {
-    fetch(url).then(
-        (reponse) => {
-            console.log(reponse);
-        }
-    );
+    fetch(url)
+        .then(reponse => reponse.json())
+        .then(data => {
+            const ul = document.createElement("ul");
+            for (const personne of data) {
+                const li = document.createElement("li");
+                li.innerText = personne.prenom + " " + personne.nom + " (" + personne.age + ")";
+                ul.appendChild(li);
+            }
+            donnees.appendChild(ul);
+        });
 
-    donnees.innerText = "Test";
 });
